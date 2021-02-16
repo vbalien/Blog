283033ccc412bea66d0936bb44a2f7f4bd54a399
Jisu Kim <webmaster@alien.moe>
2021-02-17 01:58:48 +0900

    Convert to proxy

--- core/utils/fetch.ts
+++ core/utils/fetch.ts
@@ -1,34 +1,28 @@
-import nodeFetch, {
-  RequestInfo,
-  RequestInit,
-  Request,
-  Response,
-} from "node-fetch";
+import nodeFetch, { Request, Response } from "node-fetch";
 import path from "path";
 import fs from "fs";
 
-export default async function fetch(
-  url: RequestInfo,
-  init?: RequestInit
-): Promise<Response> {
-  const request = new Request(url, init);
-
-  return new Promise(resolve => {
+export default new Proxy(nodeFetch, {
+  apply: (target, _that, args) => {
+    const [url, init] = args;
     const filePath = path.join(process.cwd(), "./dist", url.toString());
-    if (/^https?:\/\//i.test(url.toString()) || !fs.existsSync(filePath)) {
-      return nodeFetch(url, init);
-    }
-    const readStream = fs.createReadStream(filePath);
-    readStream.on("open", function () {
-      resolve(
-        new Response(readStream, {
-          url: request.url,
-          status: 200,
-          statusText: "OK",
-          size: fs.statSync(filePath).size,
-          timeout: request.timeout,
-        })
-      );
+    if (/^https?:\/\//i.test(url.toString()) || !fs.existsSync(filePath))
+      return target(url, init);
+
+    return new Promise(resolve => {
+      const request = new Request(url, init);
+      const readStream = fs.createReadStream(filePath);
+      readStream.on("open", function () {
+        resolve(
+          new Response(readStream, {
+            url: request.url,
+            status: 200,
+            statusText: "OK",
+            size: fs.statSync(filePath).size,
+            timeout: request.timeout,
+          })
+        );
+      });
     });
-  });
-}
+  },
+});
