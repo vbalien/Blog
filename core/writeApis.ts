0fa55b6e1eb454f38fe1cc3c99d2e4b5147ec37b
Jisu Kim <webmaster@alien.moe>
2021-02-17 19:00:41 +0900

    Add tags pagination

--- core/writeApis.ts
+++ core/writeApis.ts
@@ -16,26 +16,20 @@ export interface PaginationApi {
   posts: PostApi[];
 }
 export type TagsApi = {
-  tags: {
-    name: string;
-    posts: PostApi[];
-  }[];
+  tags: string[];
 };
 
 async function writePaginatorApi(
   basePath: string,
-  pages: Page[],
-  paginator: Page
+  childPages: Page[],
+  paginatorDir: string
 ): Promise<void> {
   const limit = 5;
-  const paginatorDir = path.parse(paginator.path).dir;
-  const childPages = pages.filter(
-    p => p !== paginator && RegExp(`^${paginatorDir}\\/[^\\/]*$`).test(p.path)
-  );
   const paginationApiDir = path.join(basePath, "api", paginatorDir, "page");
   const { getPageMetadata, getStaticPageTextAndImage } = await getExtractor()
     .entryPoint;
-  if (!fs.existsSync(paginationApiDir)) fs.mkdirSync(paginationApiDir);
+  if (!fs.existsSync(paginationApiDir))
+    fs.mkdirSync(paginationApiDir, { recursive: true });
 
   let pageNum = 0;
   do {
@@ -75,39 +69,48 @@ async function writePaginatorApi(
 }
 
 async function writeTagsApi(pages: Page[], basePath: string) {
+  const { getPageMetadata } = await getExtractor().entryPoint;
   const tagsApi: TagsApi = { tags: [] };
-  for (const page of pages) {
-    const { getPageMetadata, getStaticPageTextAndImage } = await getExtractor()
-      .entryPoint;
-    const pagename = normalizePagename(page.path);
-    const metadata = await getPageMetadata(pagename);
-    if (metadata?.tags) {
-      const { text: content, image } = await getStaticPageTextAndImage(
-        pagename
-      );
-      for (const tag of metadata.tags) {
-        const found = tagsApi.tags.find(t => t.name === tag);
-        if (!found)
-          tagsApi.tags.push({
-            name: tag,
-            posts: [{ metadata, content, image, ...page }],
-          });
-        else found.posts.push({ metadata, content, image, ...page });
-      }
-    }
-  }
-  const jsonPath = path.join(basePath, "tags.json");
+  const apiPath = path.join(basePath, "api");
+  const pagesWithMetadata: PageWithMetadata[] = await Promise.all(
+    pages.map(async p => {
+      const pagename = normalizePagename(p.path);
+      const metadata = await getPageMetadata(pagename);
+      return {
+        ...p,
+        metadata,
+      };
+    })
+  );
+  for (const page of pagesWithMetadata)
+    page.metadata?.tags && tagsApi.tags.push(...page.metadata?.tags);
+  tagsApi.tags = [...new Set(tagsApi.tags)];
+  const jsonPath = path.join(apiPath, "tags.json");
   const handle = await fsPromises.open(jsonPath, "w");
   await handle.writeFile(JSON.stringify(tagsApi), {});
   await handle.close();
   console.info(`Write ${jsonPath}`);
+
+  const tagPath = path.join(apiPath, "tags");
+  if (!fs.existsSync(tagPath)) fs.mkdirSync(tagPath);
+
+  for (const tag of tagsApi.tags) {
+    const childPages = pagesWithMetadata.filter(p =>
+      p.metadata?.tags?.includes(tag)
+    );
+    await writePaginatorApi(
+      basePath,
+      childPages,
+      `tags/${path.normalize(tag)}`
+    );
+  }
 }
 
 export async function writeApis(pages: Page[]): Promise<void> {
   const basePath = "./dist/";
   if (!fs.existsSync(basePath)) fs.mkdirSync(basePath);
   if (!fs.existsSync(path.join(basePath, "api")))
-    fs.mkdirSync(path.join(basePath, "api"));
+    fs.mkdirSync(path.join(basePath, "api"), { recursive: true });
 
   await writeTagsApi(pages, basePath);
 
@@ -116,7 +119,11 @@ export async function writeApis(pages: Page[]): Promise<void> {
     const apiDir = path.parse(apiPath).dir;
     if (!fs.existsSync(apiDir)) fs.mkdirSync(apiDir);
     if (/\/_paginator\.html$/.test(page.path)) {
-      await writePaginatorApi(basePath, pages, page);
+      const childPages = pages.filter(
+        p => p !== page && RegExp(`^${page}\\/[^\\/]*$`).test(p.path)
+      );
+      const paginatorDir = path.parse(page.path).dir;
+      await writePaginatorApi(basePath, childPages, paginatorDir);
     }
   }
 }
